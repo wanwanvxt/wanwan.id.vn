00226c81a214eb74c0ac5e1b27debb79055a4e31
wanwanvxt <119155820+wanwanvxt@users.noreply.github.com>
2024-04-08 09:01:59 +0700

    import VueDevTools in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,12 @@ import { fileURLToPath, URL } from 'node:url';
 
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
+import VueDevTools from 'vite-plugin-vue-devtools';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
+    VueDevTools(),
     vue()
   ],
   resolve: {

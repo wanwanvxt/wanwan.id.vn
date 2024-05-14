41d7393747284f3f5069db0e9069dd782503419c
wanwanvxt <119155820+wanwanvxt@users.noreply.github.com>
2024-05-14 18:10:51 +0700

    use import instead of link-rel

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -24,11 +24,6 @@ export default defineNuxtConfig({
         { name: 'viewport', content: 'width=device-width, initial-scale=1' },
         { charset: 'utf-8' }
       ],
-      link: [
-        { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
-        { rel: 'preconnect', href: 'https://fonts.gstatic.com', crossorigin: '' },
-        { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Roboto+Mono' }
-      ],
       htmlAttrs: { 'data-theme': 'system' }
     },
     rootId: 'app'

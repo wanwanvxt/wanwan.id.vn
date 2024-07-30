ebb68c33fc557b5a07b161faf8ea3dd8ed749f58
Vũ Xuân Trường <119155820+wanwanvxt@users.noreply.github.com>
2024-07-30 09:02:49 +0700

    drop all files

--- src/main.js
+++
@@ -1,7 +0,0 @@
-import './assets/main.css';
-import { ViteSSG } from 'vite-ssg';
-import App from './App.vue';
-
-export const createApp = ViteSSG(App, {
-  routes: [{ path: '/', component: () => import('./pages/Home.vue') }]
-});
